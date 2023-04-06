5e4e37bae77266a1f650c2628977d562f9ca0b77
Dharmendra_Kumar <dharm356@gmail.com>
2023-04-06 14:42:24 +0530

    adding project

---
+++ src/main/java/example/aws/cicd/demo/CICDDemoApplication.java
@@ -0,0 +1,36 @@
+package example.aws.cicd.demo;
+
+import example.aws.cicd.demo.model.Worker;
+import example.aws.cicd.demo.service.WorkerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@Slf4j
+public class CICDDemoApplication {
+    @Autowired
+    private WorkerService service;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CICDDemoApplication.class, args);
+    }
+
+    @GetMapping("/workers")
+    public ResponseEntity<List<Worker>> getAllWorkers() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @GetMapping("/workers/{id}")
+    public ResponseEntity<Worker> getWorkerById(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(service.getWorkerById(id));
+    }
+}
---
+++ src/main/java/example/aws/cicd/demo/service/WorkerService.java
@@ -0,0 +1,30 @@
+package example.aws.cicd.demo.service;
+
+import example.aws.cicd.demo.model.Worker;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class WorkerService {
+    private List<Worker> workers = List.of(new Worker(5, "Worker5", "e@a.com", "9876543210"),
+            new Worker(1, "Worker1", "a@a.com", "9876543210"),
+            new Worker(2, "Worker2", "b@a.com", "9876543201"),
+            new Worker(3, "Worker3", "c@a.com", "9876543120"),
+            new Worker(4, "Worker4", "d@a.com", "9876542310"));
+
+    public List<Worker> getAll() {
+        log.info("********** Fetching All list *************");
+        log.info("returning list: {}", workers);
+        return workers;
+    }
+
+    public Worker getWorkerById(Integer id) {
+        log.info("********** Fetching list by id *************");
+        Worker worker = workers.stream().filter(w-> id == w.getId()).findFirst().orElse(new Worker());
+        log.info("Worker returning: {}", worker);
+        return worker;
+    }
+}
